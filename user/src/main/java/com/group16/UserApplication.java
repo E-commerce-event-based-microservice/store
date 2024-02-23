6ff6397d3edd90441a661c4a8091677b395d2833
stephenkwan1668 <stephenkwan1668@hotmail.com>
2024-02-24 00:13:03 +0100

    Revert "Add: Integrate Spring in User service"
    This reverts commit f25004d97ca366ce6837130b2639c4652f4a039b.

--- user/src/main/java/com/group16/UserApplication.java
+++ user/src/main/java/com/group16/UserApplication.java
@@ -1,15 +1,13 @@
 package com.group16;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
 public class UserApplication 
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(UserApplication.class, args);
+        System.out.println( "Hello World!" );
     }
 }
--- user/src/main/java/com/group16/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group16.repository;
-
-import com.group16.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-
-}
