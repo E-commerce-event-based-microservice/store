43cbe7d4bd01249e5f8f81be783a22c7b4004346
Zengqhh <optiplex331@gmail.com>
2024-03-08 14:14:02 +0100

    delete

--- trade-service/src/main/java/com/group16/trade/mapper/OrderLogisticsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.group16.trade.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.group16.trade.domain.po.OrderLogistics;
-
-public interface OrderLogisticsMapper extends BaseMapper<OrderLogistics> {
-
-}
